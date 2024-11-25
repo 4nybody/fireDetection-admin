d389e03f25d9fd4756e195c80d9c8effb99c39f6
4nybody <119219336+4nybody@users.noreply.github.com>
2024-11-25 23:42:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],  // Allow Unsplash images
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
