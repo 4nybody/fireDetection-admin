cbe14b4bf456528beaf36f6dbacf5c6e1d66410d
4nybody <kahitanosecret@gmail.com>
2024-11-29 09:55:30 +0800

    Resolved merge conflict in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-<<<<<<< HEAD
     domains: ['images.unsplash.com'], // Add image domains here
-=======
-    domains: ['images.unsplash.com'],  // Allow Unsplash images
->>>>>>> 2f667a50177ba1ed73bdf1e372a6e7200ec53205
   },
 };
 
